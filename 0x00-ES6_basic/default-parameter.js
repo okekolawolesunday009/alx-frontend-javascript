37c9ac2a184d65d78d73dbe3280f7741010008fb
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-03-04 20:46:59 +0000

    alx frontend

---
+++ 0x00-ES6_basic/default-parameter.js
@@ -0,0 +1,3 @@
+export const getSumOfHods = (initialNumber, expansion1989, expansion) => {
+	(typeof expansion2019 === 'undefined') ? (typeof expansion1989 === 'undefined' ? initialNumber + 89 : initialNumber + expansion1989 + 89) : initialNumber + expansion1989 + expansion2019;
+}
