8c80642bbbc1b72127e747cd8aafa3b7c694b6f6
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-03-05 07:49:13 +0000

    fix task 2 and 3

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export const getSumOfHods = (initialNumber, expansion1989 = 89, expansion = 19) => {
+export default function getSumOfHods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
 	return initialNumber + expansion1989 + expansion2019;
 }
