0e848f30a65dbb87aada72ec1eafea1988261864
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-03-05 10:27:47 +0000

    push task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
-export connst appendToEachArrayValue = (array, appendString) => {
-	const arr = [];
-	for (const idx of array) {
-		const value = idx;
-		arr.push(appendString + value);
-	}
-	return arr;
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+  for (const idx of array) {
+    const value = idx;
+    arr.push(appendString + value);
+  }
+  return arr;
 }
