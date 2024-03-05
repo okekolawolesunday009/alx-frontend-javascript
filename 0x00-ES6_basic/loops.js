602a413a259dfe76dcb5d8b9882c5aa53934fe23
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-03-05 03:01:17 +0000

    fix task 10

---
+++ 0x00-ES6_basic/loops.js
@@ -0,0 +1,7 @@
+export connst appendToEachArrayValue = (array, appendString) => {
+	for (const idx of array) {
+		var value = array[idx];
+		array[idx] = appendString + value;
+	}
+	return array;
+}
