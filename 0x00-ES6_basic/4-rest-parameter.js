5bc767195194556d8acbdde51e42d9ec51ecb98a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-03-05 02:56:36 +0000

    fix 4 and 3

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,9 @@
 export default function returnHowManyArguments(...theArgs) {
-	let count = 0;
-	for (const arg of theArgs) {
-		count = count + 1;
-	}
-	return count;
+  let count = 0;
+  for (const arg of theArgs) {
+    if (arg) {
+      count += 1;
+    }
+  }
+  return count;
 }
