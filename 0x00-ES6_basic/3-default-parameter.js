7ea3f3ba9c8b73089a34ed2f1e4e2c90492e7425
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-03-05 07:51:11 +0000

    fix esint

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
 export default function getSumOfHods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-	return initialNumber + expansion1989 + expansion2019;
+  return initialNumber + expansion1989 + expansion2019;
 }
