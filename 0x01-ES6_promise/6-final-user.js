8a3ee81a7347a705b135f999fc61f166ae501b4b
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-03-06 22:23:11 +0000

    love js-- :( ): part3

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,14 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignUp(firstName, lastName, fileName) {
-    const userPromise = signUpUser(firstName, lastName)
-        .then(user => ({ status: 200, value: user }))
-        .catch(err => ({ status: 404, value: new Error(err) }));
+  const userPromise = signUpUser(firstName, lastName)
+    .then((user) => ({ status: 200, value: user }))
+    .catch((err) => ({ status: 404, value: new Error(err) }));
 
-    const photoPromise = uploadPhoto(fileName)
-        .then(photo => ({ status: 200, value: photo }))
-        .catch(err => ({ status: 404, value: new Error(err) }));
+  const photoPromise = uploadPhoto(fileName)
+    .then((photo) => ({ status: 200, value: photo }))
+    .catch((err) => ({ status: 404, value: new Error(err) }));
 
-    return Promise.all([userPromise, photoPromise]);
+  return Promise.all([userPromise, photoPromise]);
 }
-
